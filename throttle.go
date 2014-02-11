1e4727ebc2e2a18653e44fd5512596065c8a7bb7
Martin Angers <martin.n.angers@gmail.com>
2014-02-10 19:12:17 -0500

    throttler with tests

--- throttle.go
+++ throttle.go
@@ -6,21 +6,25 @@ import (
 )
 
 type Throttle struct {
-	delay   time.Duration
+	delay  time.Duration
+	bursts int
+
 	wg      sync.WaitGroup
 	bucket  chan chan bool
 	stop    chan struct{}
 	mu      sync.Mutex
 	started bool
+	muproc  sync.Mutex
 }
 
 func New(freq Delayer, bursts int) *Throttle {
-	return &Throttle{delay: freq.Delay()}
+	return &Throttle{delay: freq.Delay(), bursts: bursts}
 }
 
 func (t *Throttle) Func(fn func()) func() {
 	t.start()
 	return func() {
+		t.wg.Add(1)
 		defer t.wg.Done()
 		ch := t.try()
 		ok := <-ch
@@ -30,11 +34,26 @@ func (t *Throttle) Func(fn func()) func() {
 	}
 }
 
+func (t *Throttle) Close() {
+	// Make sure no new calls get through
+	close(t.stop)
+	// Wait for pending calls (drain)
+	t.wg.Wait()
+	// Safely close the bucket, so that t.process can exit
+	close(t.bucket)
+	// Wait for t.process to exit
+	t.muproc.Lock()
+	t.muproc.Unlock()
+}
+
 func (t *Throttle) start() {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	if !t.started {
-		t.bucket = make(chan chan bool, 1)
+		if t.bursts <= 1 {
+			t.bursts = 1
+		}
+		t.bucket = make(chan chan bool, t.bursts)
 		t.stop = make(chan struct{})
 		t.started = true
 		go t.process()
@@ -50,6 +69,7 @@ func (t *Throttle) try() <-chan bool {
 		return ch
 	default:
 	}
+
 	select {
 	case t.bucket <- ch:
 		return ch
@@ -62,6 +82,9 @@ func (t *Throttle) try() <-chan bool {
 
 func (t *Throttle) process() {
 	var after <-chan time.Time
+
+	t.muproc.Lock()
+	defer t.muproc.Unlock()
 	for v := range t.bucket {
 		if after != nil {
 			<-after
--- throttle_test.go
+++ throttle_test.go
@@ -1,7 +1,48 @@
 package throttled
 
-import "testing"
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestThrottleFuncCall(t *testing.T) {
+	ok := false
+	th := New(PerSec(1), 0)
+	fn := th.Func(func() {
+		ok = true
+	})
+	fn()
+
+	if !ok {
+		t.Error("expected func to be called")
+	}
+}
 
 func TestThrottleFunc(t *testing.T) {
-
+	var ts []time.Time
+	var cnt int
+	var mu sync.Mutex
+	n := 5
+	th := New(PerSec(4), n-1) // Timestamp should be 250ms apart, one call should be dropped
+	fn := th.Func(func() {
+		mu.Lock()
+		defer mu.Unlock()
+		ts = append(ts, time.Now())
+		cnt++
+	})
+	for i := 0; i < n; i++ {
+		go fn()
+	}
+	time.Sleep(time.Second)
+	mu.Lock()
+	defer mu.Unlock()
+	if cnt != n-1 {
+		t.Errorf("expected %d calls, got %d", n-1, cnt)
+	}
+	for i := 0; i < cnt-1; i++ {
+		if (ts[i+1].Sub(ts[i]) < 250*time.Millisecond) || (ts[i+1].Sub(ts[i]) > 270*time.Millisecond) {
+			t.Errorf("expected calls to be 250ms apart")
+		}
+	}
 }
